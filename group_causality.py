94d005e3ba91a4d5fdf1dd2e9b3d5360e971c85c
测试账户 <uais_common@uais.lzu.edu.cn>
2017-04-19 16:50:31 +0800

    binomial test for group causality analysis

--- group_causality.py
+++ group_causality.py
@@ -2,22 +2,53 @@
 matrices less than 10 were set as zeros. And was stored in <condition>_<fmin>_<fmax>.npy
 Using FDR to corrected the group connections.  
 '''
-import os
+import os, glob
 import numpy as np
 from mne.stats import fdr_correction
 import scipy.stats as stats
 
 subjects_dir = os.environ['SUBJECTS_DIR']
-cau_path = subjects_dir+'/fsaverage/stcs/causality' 
-X = np.load(cau_path + '/LLst_18_30Hz.npy')
+cau_path = subjects_dir+'/fsaverage/stcs/' 
+#st_list = ['LLst', 'RRst', 'RLst',  'LRst']
+st_list = ['LLst']
+# Stats set-up
+prob = 0.5           # we assume the probability of chance
+alpha = 0.05         # False alarm rate
+# we need to load all data in one array as we later perform FDR correction
+# the content of the data should be 1 (above surrogate) or 0 (below surrogate threshold
 
-pval = 1 - stats.binom.cdf(X, 13, 0.5)
-reject_fdr, pval_fdr = fdr_correction(pval, alpha=0.05, method='indep')
-n_rows = X.shape[0]
-for i in range(n_rows):
-    for j in range(n_rows):
-        if i == j:
-            continue
-        if reject_fdr[i, j] == False:
-            X[i, j] = 0
+for evt_st in st_list:
+    X = []
+    fnsig_list = glob.glob(cau_path + '/*[0-9]/sig_cau/%s_sig_con_band.npy' %evt_st)
+    for f in fnsig_list:
+        sig_cau = np.load(f)
+        print sig_cau.shape[-1]
+        X.append(sig_cau)
+        
+    X = np.array(X)
+    n_subj, n_freqs, n_rois, _ = np.shape(X)
+    
+    # count number of subjects showing a connection above surrogate
+    counts = X.sum(axis=0)      # sum counts across subjects
+    
+    # compute minimum number of subjects needed for being significant
+    thresh = stats.binom.ppf((1-alpha), n_subj, prob) + 1  # must be the next upper integer
+    
+    # zero those which are below the minimum expected number of subjects
+    counts[counts < thresh] = 0   # not really necessary as we will reject this later with pval
+    
+    # compute p-values
+    pval = 1 - stats.binom.cdf(counts, n_subj, prob)
+    
+    # use FDR to correct for multiple comparison
+    reject_mask = np.zeros(pval.shape)
+    for iroi in range(n_rois):
+       reject_mask[:, iroi, :], _ = fdr_correction(pval[:, iroi, :], alpha) 
+    #reject_mask, pval_fdr = fdr_correction(pval, alpha)
+    
+    
+    # remove non-significant connections
+    for isubj in range(n_subj):
+        X[isubj] *= reject_mask
+
 
